3cf2726a439386701aa40f8c0334727866dc5305
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2022-12-19 22:20:03 +0200

    Rewrite randoming teams

--- Src/Bot/Matchmaking/TeamCreation/RandomTeamsStrategy.py
+++ Src/Bot/Matchmaking/TeamCreation/RandomTeamsStrategy.py
@@ -5,7 +5,8 @@ import random
 from collections.abc import Iterator
 
 from Src.Bot.Converters.TeamCreationStrategyConverter import TeamCreationStrategyConverter
-from Src.Bot.Logic.ScrimTeamsManager import ScrimTeamsManager
+from Src.Bot.DataClasses.Scrim import Scrim
+from Src.Bot.DataClasses.Team import PARTICIPANTS
 from Src.Bot.Matchmaking.TeamCreation.TeamCreationStrategy import TeamCreationStrategy
 
 
@@ -20,14 +21,15 @@ def _next_team_iterator(team_count: int) -> Iterator[int]:
 @TeamCreationStrategyConverter.register("random")
 class RandomTeamsStrategy(TeamCreationStrategy):
 
-    def _create_teams_hook(self, teams_manager: ScrimTeamsManager):
-        players = teams_manager.get_standard_teams()[0].members.copy()
-        teams = [[] for _ in range(teams_manager.game.team_count)]
-        team_iterator = _next_team_iterator(teams_manager.game.team_count)
+    def _create_teams_hook(self, scrim: Scrim):
+        players = next(team.team for team in scrim.teams if team.team.name == PARTICIPANTS).members.copy()
+        teams = [[] for _ in range(scrim.game.team_count)]
+        team_iterator = _next_team_iterator(scrim.game.team_count)
         while players:
             team_index = next(team_iterator)
             teams[team_index].append(players.pop(random.randint(0, len(players) - 1)))
 
         for team_index, team in enumerate(teams):
             for player in team:
-                teams_manager.set_team(team_index, player)
+                self._team_service.remove_from_team(scrim, player)
+                self._team_service.add_to_team(scrim, player, f"Team {team_index + 1}")
--- Test/UnitTest/Bot/Matchmaking/TeamCreation/TestRandomTeamsStrategy.py
+++ Test/UnitTest/Bot/Matchmaking/TeamCreation/TestRandomTeamsStrategy.py
@@ -4,38 +4,49 @@ __author__ = "Eetu Asikainen"
 import unittest
 from unittest.mock import MagicMock, AsyncMock
 
+from Src.Bot.DataClasses.ParticipantTeam import ParticipantTeam
+from Src.Bot.DataClasses.Team import PARTICIPANTS, SPECTATORS, QUEUE
 from Src.Bot.Matchmaking.TeamCreation.RandomTeamsStrategy import RandomTeamsStrategy
 from Utils.TestBases.AsyncUnittestBase import AsyncUnittestBase
 
 
-@unittest.skip("Waiting for team creation rewrite")
+def _create_team(name: str) -> ParticipantTeam:
+    mock_participant_team = MagicMock()
+    mock_team = MagicMock()
+    mock_team.members = []
+    mock_team.name = name
+    mock_participant_team.max_size = 0
+    mock_participant_team.team = mock_team
+    return mock_participant_team
+
+
 class TestRandomTeamsStrategy(AsyncUnittestBase):
 
     def setUp(self) -> None:
-        self.creation_strategy = RandomTeamsStrategy()
-        self.scrim_manager = MagicMock()
-        self.teams_manager = MagicMock()
-        self.scrim_manager.teams_manager = self.teams_manager
-        self.scrim_manager.message = AsyncMock()
+        self.converter = MagicMock()
+        self.creation_strategy = RandomTeamsStrategy(self.converter)
+        self.scrim = MagicMock()
+        self.scrim.teams = [_create_team(name) for name in (PARTICIPANTS, SPECTATORS, QUEUE, "Team 1", "Team 2")]
+        self.message = AsyncMock()
         self.all_players = []
-        self.mock_participants = MagicMock()
-        self.mock_participants.members = self.all_players
-        self.teams_manager.get_standard_teams.return_value = [self.mock_participants]
+        self.scrim.teams[0].team.members = self.all_players
 
     async def test_create_teams_when_given_strictly_sized_teams_then_all_teams_filled(self):
         self._setup_game(5)
         self._add_players(10)
-        await self.creation_strategy.create_teams(self.scrim_manager)
-        calls = self.teams_manager.set_team.call_args_list
-        self.assertEqual(10, len(calls))
-        self.teams_manager.clear_teams.assert_called()
+        await self.creation_strategy.create_teams(self.scrim, self.message)
+        remove_calls = self.converter.remove_from_team.call_args_list
+        add_calls = self.converter.add_to_team.call_args_list
+        self.assertEqual(10, len(remove_calls))
+        self.assertEqual(10, len(add_calls))
+        self.converter.clear_teams.assert_called()
 
     def _setup_game(self, min_players: int, max_players: int = None, team_count: int = 2):
         mock_game = MagicMock()
         mock_game.min_team_size = min_players
         mock_game.max_team_size = min_players if max_players is None else max_players
         mock_game.team_count = team_count
-        self.teams_manager.game = mock_game
+        self.scrim.game = mock_game
 
     def _add_players(self, amount: int):
         for _ in range(amount):
